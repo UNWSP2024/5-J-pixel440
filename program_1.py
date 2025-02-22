3d903991d00e3ce5e257f5ca9380fabf580e2c45
J-pixel440 <Joshplath1@gmail.com>
2025-02-21 21:50:07 -0500

    Update program_1.py
    By Josh Plath, 2.21.2025

--- program_1.py
+++ program_1.py
@@ -4,16 +4,22 @@
 # Miles = kilometers x 0.6214.   
 # The conversion must be done as a function with input and output.
 
-
+    ######################
 def kilometer_conversion(kilometers):    
     miles = 0.0
-    ######################
-    # WRITE YOUR CODE HERE
+    miles=kilometers*0.6214
+    return miles
+
+kilometers=float(input("Enter the number of kilometers:"))
+
+miles=kilometer_conversion(kilometers)
+
+print(f"{kilometers} kilometers equals {miles} miles.")
     ######################    
 
 
     # Return the variable to the calling function
-    return miles
+    #return miles (***NOTE*** I moved this return command into the function; my compiler wouldn't allow me to run the code unless I placed the return in the function.)
 
 #### This piece of the code has been done for you,
 #### you only need to worry about the actual kilometer
