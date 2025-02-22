074ebe7756c7e8eedb2dedf64b74cedc4176911a
J-pixel440 <Joshplath1@gmail.com>
2025-02-21 22:18:56 -0500

    Update program_2.py
    By Josh Plath, 2.21.2025

--- program_2.py
+++ program_2.py
@@ -6,7 +6,24 @@
 # + 129
 
 # ------
+import random
 
+def student_quiz():
+    n1=random.randint(1,300)
+    n2=random.randint(1,300)
+
+    print(f"{n1}\n+{n2}\n------")
+
+    student_input=int(input("Answer:"))
+
+    solution=n1+n2
+
+    if student_input==solution:
+        print("Correct, nice job!")
+    else:
+        print(f"Not quite, the correct answer is {solution}.")
+
+student_quiz()
 # The program should allow the student to enter the answer.  
 # If the answer is correct, a message of congratulations should be displayed.  
 # If the answer is incorrect a message showing the correct answer should be displayed.  
